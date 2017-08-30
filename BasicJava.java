f4296871c2159eb3b6ff7d3861712208ee90a8b4
mateogolf <rodriguezml11@mail.wlu.edu>
2017-08-30 11:05:20 -0400

    Basic 13 Algos in Java

--- BasicJava.java
+++ BasicJava.java
@@ -20,22 +20,19 @@ public class BasicJava{
         // int[] arr = {1, 3, 5, 7};
         // bj.greaterThanY(arr,3);
         // Square the values
-        int[] x = {1, 5, 10, -2};
-        System.out.println(bj.squareArray(x));
-        //     // Given any array x, say [1, 5, 10, -2], write a method that multiplies each value in the array by itself. When the method is done, the array x should have values that have been squared, say [1, 25, 100, 4].
-        // }
+        // int[] x = {1, 5, 10, -2};
+        // System.out.println(bj.squareArray(x));
         // // Eliminate Negative Numbers
-        // public Array removeNeginArray(Array x){
-        //     // Given any array x, say [1, 5, 10, -2], write a method that replaces any negative number with the value of 0. When the method is done, x should have no negative values, say [1, 5, 10, 0].
-        // }
+        // int[] x = {1, 5, 10, -2};
+        // int[] newArr = bj.removeNeginArray(x);
+        // bj.iterArray(newArr);
         // // Max, Min, and Average
-        // public Array findMaxMinAvg(Array x){
-        //     // Given any array x, say [1, 5, 10, -2], write a method that returns an array with the maximum number in the array, the minimum value in the array, and the average of the values in the array. The returned array should be three elements long and contain: [MAXNUM, MINNUM, AVG]
-        // }
+        // double[] mma = bj.findMaxMinAvg(x);
+        // bj.iterArray(mma);
         // // Shifting the Values in the Array
-        // public Array shiftVals(Array x){
-        //     // Given any array x, say [1, 5, 10, 7, -2], write a method that shifts each number by one to the front. For example, when the method is done, an x of [1, 5, 10, 7, -2] should become [5, 10, 7, -2, 0].
-        // }
+        int[] x = {1, 5, 10, 7, -2};
+        int[] newArr = bj.shiftVals(x);
+        bj.iterArray(newArr);
     }
 
     //Print 1-255
@@ -65,6 +62,11 @@ public class BasicJava{
     }
     // Iterating through an array
     public void iterArray(int[] x){
+        for(int i=0;i<x.length;i++){
+            System.out.println(x[i]);
+        }
+    }
+    public void iterArray(double[] x){
         for(int i=0;i<x.length;i++){
             System.out.println(x[i]);
         }
@@ -116,15 +118,39 @@ public class BasicJava{
         return newArr;
     }
     // Eliminate Negative Numbers
-    public Array removeNeginArray(Array x){
+    public int[] removeNeginArray(int[] x){
         // Given any array x, say [1, 5, 10, -2], write a method that replaces any negative number with the value of 0. When the method is done, x should have no negative values, say [1, 5, 10, 0].
+        for(int i=0;i<x.length;i++){
+            if(x[i] < 0){
+                x[i] = 0;
+            }
+        }
+        return x;
     }
-    // // Max, Min, and Average
-    // public Array findMaxMinAvg(Array x){
-    //     // Given any array x, say [1, 5, 10, -2], write a method that returns an array with the maximum number in the array, the minimum value in the array, and the average of the values in the array. The returned array should be three elements long and contain: [MAXNUM, MINNUM, AVG]
-    // }
-    // // Shifting the Values in the Array
-    // public Array shiftVals(Array x){
-    //     // Given any array x, say [1, 5, 10, 7, -2], write a method that shifts each number by one to the front. For example, when the method is done, an x of [1, 5, 10, 7, -2] should become [5, 10, 7, -2, 0].
-    // }
+    // Max, Min, and Average
+    public double[] findMaxMinAvg(int[] x){
+        // Given any array x, say [1, 5, 10, -2], write a method that returns an array with the maximum number in the array, the minimum value in the array, and the average of the values in the array. The returned array should be three elements long and contain: [MAXNUM, MINNUM, AVG]
+        double[] result = {x[0],x[0],0};// max,min,avg
+        for(Integer i: x){
+            result[2] = result[2] + i;
+            if(result[0] < i){//if max < val replace
+                result[0] = i;
+            }
+            if(result[1] > i){//if min > val replace
+                result[1] = i;
+            }
+        }
+        result[2] = result[2] / x.length;
+        return result;
+    }
+    // Shifting the Values in the Array
+    public int[] shiftVals(int[] x){
+        // Given any array x, say [1, 5, 10, 7, -2], write a method that shifts each number by one to the front. For example, when the method is done, an x of [1, 5, 10, 7, -2] should become [5, 10, 7, -2, 0].
+        // ArrayList<Integer> newArr = new ArrayList<Integer>();
+        for(int i=0;i<x.length-1;i++){
+            x[i] = x[i+1];
+        }
+        x[x.length-1] = 0;
+        return x;
+    }
 }
